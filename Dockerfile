77b2a8df44d4fe9a6abe736cc853302cb8943c4e
Pythack <59161870+Pythack@users.noreply.github.com>
2022-03-12 12:53:52 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# 1. This tells docker to use the Rust official image
+FROM rust:1.49
+
+# 2. Copy the files in your machine to the Docker image
+COPY ./ ./
+
+# Build your program for release
+RUN cargo build
+RUN cp ./target/debug/permscan /bin
+RUN rm -r target
+RUN rm -r src
+RUN rm Cargo.*
+RUN rm Dockerfile
+
+# Run the binary
+#CMD ["cp ./target/release/permscan /bin"]
+#EXPOSE 3030
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use regex::Regex;
+use std::fs;
+use std::path::PathBuf;
+use std::process::Command;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Permission scanner",
+    about = "Scan a directory for files that match permission criterias"
+)]
+struct Opt {
+    #[structopt(short, help = "Specify the owner of the file in the format user:group")]
+    owner: Option<String>,
+}
+
+fn run_command(command: String, args: String) -> String {
+    let output = Command::new(command).arg(args).output().expect("");
+    let stdout = String::from_utf8(output.stdout);
+    let stdout = match stdout {
+        Err(_e) => String::from(""),
+        Ok(out) => out,
+    };
+    return stdout;
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    let files = run_command(String::from("ls"), String::from("-la"));
+    let lines = files.split("\n");
+    if opt.owner.is_some() {
+        let owner = match opt.owner {
+            None => String::from(""),
+            Some(owner) => owner.replace(":", " "),
+        };
+        let retext = String::from(r"[drwxt\-]{10}[ 0-9]* ") + &*owner + r" (.|\n)*";
+        println!("{}", retext);
+        let re = Regex::new(&regex::escape(&retext)).unwrap();
+        println!("{}", re);
+        for line in lines.skip(1) {
+            let line = String::from(line);
+            if re.is_match(&line) {
+                println!("{}", line);
+            }
+        }
+    }
+}
