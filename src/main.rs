43ef6242ae3b441e3628c37154b54ff69d0a4995
gmnsii <gmnsii@users.noreply.github.com>
2022-03-12 11:19:23 -0800

    optimized code

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(
     name = "Permission scanner",
-    about = "Scan a directory for files that match permission criterias"
+    about = "Scan a directory for files that match permission criteria"
 )]
 struct Opt {
     #[structopt(
@@ -21,7 +21,7 @@ struct Opt {
 
     #[structopt(
         short,
-        help = "If present, will return the list of files that match at least one criteria, else return the list of files that match all criterias"
+        help = "If present, will return the list of files that match at least one criteria, else return the list of files that match all criteria"
     )]
     merge: bool,
 }
@@ -29,15 +29,15 @@ struct Opt {
 fn run_command(command: String, args: String) -> String {
     let output = Command::new(command).arg(args).output().expect("");
     let stdout = String::from_utf8(output.stdout);
-    let stdout = match stdout {
+
+    match stdout {
         Err(_e) => String::from(""),
         Ok(out) => out,
-    };
-    return stdout;
+    }
 }
 
 fn get_based_on_owner(files: String, owner: String) -> Vec<String> {
-    let lines = files.split("\n");
+    let lines = files.split('\n');
     let mut temp_lines: Vec<String> = Vec::new();
     let retext = String::from(r"^[drwxt\-]{10}[ 0-9]* *") + &*owner + r" (.|\n)*$";
     let re = Regex::new(&retext).unwrap();
@@ -47,11 +47,11 @@ fn get_based_on_owner(files: String, owner: String) -> Vec<String> {
             temp_lines.push(line);
         }
     }
-    return temp_lines;
+    temp_lines
 }
 
 fn get_based_on_user(files: String, user: String) -> Vec<String> {
-    let lines = files.split("\n");
+    let lines = files.split('\n');
     let mut temp_lines: Vec<String> = Vec::new();
     let retext = String::from(r"^[drwxt\-]") + &user + r"(.|\n)*$";
     let re = Regex::new(&retext).unwrap();
@@ -61,20 +61,20 @@ fn get_based_on_user(files: String, user: String) -> Vec<String> {
             temp_lines.push(line);
         }
     }
-    return temp_lines;
+    temp_lines
 }
 
 fn main() {
     let opt = Opt::from_args();
     let files = run_command(String::from("ls"), String::from("-la"));
-    let files_owner_check = files.clone();
-    let files_user_check = files.clone();
+    let files_owner_check = files;
+    let files_user_check = files_owner_check.clone();
     let mut all_lines: Vec<Vec<String>> = Vec::new();
     let mut temp_lines: Vec<String> = Vec::new();
     if opt.owner.is_some() {
         let owner = match opt.owner {
             None => String::from(""),
-            Some(owner) => owner.replace(":", " *"),
+            Some(owner) => owner.replace(':', " *"),
         };
         if opt.merge {
             temp_lines.extend(get_based_on_owner(files_owner_check, owner).iter().cloned());
@@ -100,20 +100,17 @@ fn main() {
         for line in temp_lines {
             println!("{}", line);
         }
-    } else {
-        if all_lines.len() > 0 {
-            let reference_lines = all_lines[0].clone();
-            let mut final_lines: Vec<Vec<String>> = Vec::new();
-            final_lines.push(reference_lines);
-            all_lines.remove(0);
-            for lines_set in &all_lines {
-                let final_lines_len = final_lines.len();
-                final_lines.push(final_lines[final_lines_len - 1].intersect(lines_set.to_vec()));
-            }
+    } else if !all_lines.is_empty() {
+        let reference_lines = all_lines[0].clone();
+        let mut final_lines: Vec<Vec<String>> = vec![reference_lines];
+        all_lines.remove(0);
+        for lines_set in &all_lines {
             let final_lines_len = final_lines.len();
-            for line in &final_lines[final_lines_len - 1] {
-                println!("{}", line);
-            }
+            final_lines.push(final_lines[final_lines_len - 1].intersect(lines_set.to_vec()));
+        }
+        let final_lines_len = final_lines.len();
+        for line in &final_lines[final_lines_len - 1] {
+            println!("{}", line);
         }
     }
 }
