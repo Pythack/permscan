ce14768580e09e1d51429fc47e3e1d27ce7ec847
Pythack <59161870+Pythack@users.noreply.github.com>
2022-03-27 16:39:41 +0200

    fixed bugs

--- src/misc.rs
+++ src/misc.rs
@@ -1,6 +1,8 @@
 use reqwest::blocking::Client;
 use std::process::Command;
 
+const VERSION: &str = "v2.2.1";
+
 pub fn run_command(command: String, args: String, path: String) -> String {
     let output = Command::new(command)
         .arg(args)
@@ -39,8 +41,8 @@ pub fn check_for_newer_version() {
                 serde_json::from_str(&response).expect("Failed to parse");
             let latest = json.as_array().unwrap();
             if !latest.is_empty() {
-                if latest[0]["tag_name"] != "v2.1.1" {
-                    println!("\x1b[93mNew version available: {}! Visit this url: {}\x1b[0m", rem_first(latest[0]["tag_name"].as_str().unwrap(), "v"), latest[0]["url"].as_str().unwrap());
+                if latest[0]["tag_name"] != VERSION {
+                    println!("\x1b[93mNewer version available: {}! Visit this url: {}\x1b[0m", rem_first(latest[0]["tag_name"].as_str().unwrap(), "v"), latest[0]["url"].as_str().unwrap());
                 } else {
                     println!("\x1b[92mYou have the latest version!\x1b[0m");
                 }
