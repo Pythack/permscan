80de5847bc9dfd6ea5efaef8245890a4f7930ef0
gmnsii <95436780+gmnsii@users.noreply.github.com>
2022-03-28 05:36:15 -0700

    Replaces version names with numbers

--- installer.sh
+++ installer.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-echo "What version of permscan do you need ? (linux-gnu, linux-musl macos-arm, macos-w86_64)"
+echo "What version of permscan do you need ? (1, 2, 3, 4)
+1. linux-gnu
+2. linux-musl
+3. macos-arm
+4. macos-x86_64"
 read -r version
-if [ "$version" = "linux-gnu" ]; then
+if [ "$version" = "1" ]; then
     wget https://github.com/Pythack/permscan/releases/latest/download/permscan-x86_64-unknown-linux-gnu.tar.gz
     tar -xzvf permscan-x86_64-unknown-linux-gnu.tar.gz
     sudo mv permscan-x86_64-unknown-linux-gnu/permscan /bin
     rm -rf permscan-x86_64-unknown-linux-gnu.tar.gz
     rm -rf permscan-x86_64-unknown-linux-gnu
     rm -f installer.sh
-elif [ "$version" = "linux-musl" ]; then
+elif [ "$version" = "2" ]; then
     wget https://github.com/Pythack/permscan/releases/latest/download/permscan-x86_64-unknown-linux-musl.tar.gz
     tar -xzvf permscan-x86_64-unknown-linux-musl.tar.gz
     sudo mv permscan-x86_64-unknown-linux-musl/permscan /bin
     rm -rf permscan-x86_64-unknown-linux-musl.tar.gz
     rm -rf permscan-x86_64-unknown-linux-musl
     rm -f installer.sh
-elif [ "$version" = "macos-arm" ]; then
+elif [ "$version" = "3" ]; then
     wget https://github.com/Pythack/permscan/releases/download/v2.2.6/permscan-aarch64-apple-darwin.zip
     unzip permscan-aarch64-apple-darwin.zip
     sudo mv permscan-aarch64-apple-darwin/permscan /usr/local/bin
@@ -24,7 +28,7 @@ elif [ "$version" = "macos-arm" ]; then
     rm -rf permscan-aarch64-apple-darwin
     rm -rf __MACOSX
     rm -f installer.sh
-elif [ "$version" = "macos-x86_64" ]; then
+elif [ "$version" = "4" ]; then
     wget https://github.com/Pythack/permscan/releases/download/v2.2.6/permscan-x86_64-apple-darwin.zip
     unzip permscan-x86_64-apple-darwin.zip
     sudo mv permscan-x86_64-apple-darwin/permscan /usr/local/bin
