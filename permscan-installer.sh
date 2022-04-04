c420359a0381322c9a1721b8742a0c263630413e
gmnsii <95436780+gmnsii@users.noreply.github.com>
2022-04-04 05:14:44 -0700

    automated installer

--- permscan-installer.sh
+++ permscan-installer.sh
@@ -1,47 +1,55 @@
 #!/bin/bash
 
-echo "What version of permscan do you need ? (1, 2, 3, 4)
-1. linux-gnu
-2. linux-musl
-3. macos-arm
-4. macos-x86_64
-5. build it yourself (recommended if your architecture is not yet supported) (rustlang must be installed)"
-read -r version
-if [ "$version" = "1" ]; then
-    wget https://github.com/Pythack/permscan/releases/latest/download/permscan-x86_64-unknown-linux-gnu.tar.gz
-    tar -xzvf permscan-x86_64-unknown-linux-gnu.tar.gz
-    sudo mv permscan-x86_64-unknown-linux-gnu/permscan /bin
-    rm -rf permscan-x86_64-unknown-linux-gnu.tar.gz
-    rm -rf permscan-x86_64-unknown-linux-gnu
-elif [ "$version" = "2" ]; then
-    wget https://github.com/Pythack/permscan/releases/latest/download/permscan-x86_64-unknown-linux-musl.tar.gz
-    tar -xzvf permscan-x86_64-unknown-linux-musl.tar.gz
-    sudo mv permscan-x86_64-unknown-linux-musl/permscan /bin
-    rm -rf permscan-x86_64-unknown-linux-musl.tar.gz
-    rm -rf permscan-x86_64-unknown-linux-musl
-elif [ "$version" = "3" ]; then
-    wget https://github.com/Pythack/permscan/releases/latest/download/permscan-aarch64-apple-darwin.zip
-    unzip permscan-aarch64-apple-darwin.zip
-    sudo mv permscan-aarch64-apple-darwin/permscan /usr/local/bin
-    rm -rf permscan-aarch64-apple-darwin.zip
-    rm -rf permscan-aarch64-apple-darwin
-    rm -rf __MACOSX
-elif [ "$version" = "4" ]; then
-    wget https://github.com/Pythack/permscan/releases/latest/download/permscan-x86_64-apple-darwin.zip
-    unzip permscan-x86_64-apple-darwin.zip
-    sudo mv permscan-x86_64-apple-darwin/permscan /usr/local/bin
-    rm -rf permscan-x86_64-apple-darwin.zip
-    rm -rf permscan-x86_64-apple-darwin
-    rm -rf __MACOSX
-elif [ "$version" = "5" ]; then
+BUILD="false"
+while getopts 'u' flag; do
+    case "${flag}" in
+    u) BUILD="true" ;;
+    *) echo 'permscan: update: illegal argument' ;;
+    esac
+done
+
+if [ "$BUILD" = "false" ]; then
+    OS=$(uname)
+    if [ "$OS" = "Linux" ]; then
+        OSTYPE=$(uname -o)
+        if [ "$OSTYPE" = "GNU/Linux" ]; then
+            wget https://github.com/Pythack/permscan/releases/latest/download/permscan-x86_64-unknown-linux-gnu.tar.gz
+            tar -xzvf permscan-x86_64-unknown-linux-gnu.tar.gz
+            sudo mv permscan-x86_64-unknown-linux-gnu/permscan /bin
+            rm -rf permscan-x86_64-unknown-linux-gnu.tar.gz
+            rm -rf permscan-x86_64-unknown-linux-gnu
+        elif [ "$OSTYPE" = "Linux" ]; then
+            wget https://github.com/Pythack/permscan/releases/latest/download/permscan-x86_64-unknown-linux-musl.tar.gz
+            tar -xzvf permscan-x86_64-unknown-linux-musl.tar.gz
+            sudo mv permscan-x86_64-unknown-linux-musl/permscan /bin
+            rm -rf permscan-x86_64-unknown-linux-musl.tar.gz
+            rm -rf permscan-x86_64-unknown-linux-musl
+        else
+            echo "permscan: intaller: failed"
+        fi
+    elif [ "$OS" = "Darwin" ]; then
+        OSTYPE=$(uname -m)
+        if [ "$OSTYPE" = "arm64" ]; then
+            wget https://github.com/Pythack/permscan/releases/latest/download/permscan-aarch64-apple-darwin.zip
+            unzip permscan-aarch64-apple-darwin.zip
+            sudo mv permscan-aarch64-apple-darwin/permscan /usr/local/bin
+            rm -rf permscan-aarch64-apple-darwin.zip
+            rm -rf permscan-aarch64-apple-darwin
+            rm -rf __MACOSX
+        elif [ "$OSTYPE" = "x86_64" ]; then
+            wget https://github.com/Pythack/permscan/releases/latest/download/permscan-x86_64-apple-darwin.zip
+            unzip permscan-x86_64-apple-darwin.zip
+            sudo mv permscan-x86_64-apple-darwin/permscan /usr/local/bin
+            rm -rf permscan-x86_64-apple-darwin.zip
+            rm -rf permscan-x86_64-apple-darwin
+            rm -rf __MACOSX
+        fi
+    fi
+elif [ "$BUILD" = "true" ]; then
     git clone https://github.com/Pythack/permscan
     cd permscan || exit
     cargo build --release
     sudo mv ./target/release/permscan /usr/local/bin
     cd ..
     rm -rf permscan
-else
-    echo "permscan: installer: unknown version"
-    exit 1
 fi
-rm -f permscan-installer.sh
