2d47a7f3d8b4f38782a1896bc93e8992ab577f75
gmnsii <95436780+gmnsii@users.noreply.github.com>
2022-11-06 14:25:25 -0800

    Update output.rs

--- src/output.rs
+++ src/output.rs
@@ -1,11 +1,7 @@
-//! functions to output permscan output
-
 use array_tool::vec::*;
 use regex::Regex;
 use std::io::Write;
 
-#[path = "./colors.rs"]
-mod colors;
 #[path = "./types.rs"]
 mod types;
 
@@ -13,75 +9,54 @@ use types::Result;
 
 use crate::PermscanOutput;
 
-// A wrapper around print_results_merge() and print_results_nomerge()
-// that call one of them based on the type of the output
-pub fn print_results(lines: PermscanOutput, recursive: &bool) -> Result<()> {
+pub fn print_results(lines: &mut PermscanOutput, recursive: bool) -> Result<()> {
     match lines {
-        PermscanOutput::NoMerge(lines) => {
-            print_results_nomerge(lines, recursive)
-        }
+        PermscanOutput::NoMerge(lines) => print_results_default(lines, recursive),
         PermscanOutput::Merge(lines) => print_results_merge(lines, recursive),
     }
 }
 
-// Prints results. Called when opt.merge is false
-fn print_results_nomerge(
-    mut lines: Vec<Vec<&str>>,
-    recursive: &bool,
-) -> Result<()> {
-    // when using the recursive option, we have lines that tells us what
-    // folder we are into. We want to be able to match these lines to
-    // print them in color
-    let sub_dir_text = Regex::new(&String::from(r"^(.+)/*([^/]+)*:$")).unwrap();
-
-    // lock stdout manually for better performances since we are going to print
-    // to it a lot
-    let stdout = std::io::stdout();
-    let mut lock = stdout.lock();
-
-    // TODO: comment this
+fn print_results_default(lines: &mut Vec<Vec<&str>>, recursive: bool) -> Result<()> {
     if !lines.is_empty() {
         let reference_lines = lines[0].clone();
         let mut final_lines: Vec<Vec<&str>> = vec![reference_lines];
         lines.remove(0);
-        for lines_set in &lines {
-            final_lines.push(
-                final_lines[final_lines.len() - 1]
-                    .intersect(lines_set.to_vec()),
-            );
+        for lines_set in lines {
+            final_lines.push(final_lines[final_lines.len() - 1].intersect(lines_set.to_vec()));
         }
-        for line in &final_lines[final_lines.len() - 1] {
-            if *recursive && sub_dir_text.is_match(line) {
-                writeln!(lock, "{}{}{}", colors::GREEN, line, colors::RESET)?;
-            } else {
-                writeln!(lock, "{}", line)?;
-            }
-        }
+        print_lines(&final_lines[final_lines.len() - 1], recursive)?;
     }
     Ok(())
 }
 
-// Prints results. Called when opt.merge is true
-fn print_results_merge(lines: Vec<&str>, recursive: &bool) -> Result<()> {
-    // when using the recursive option, we have lines that tells us what
-    // folder we are into. We want to be able to match these lines to
-    // print them in color
+fn print_results_merge(lines: &Vec<&str>, recursive: bool) -> Result<()> {
+    // Remove items that appears multiple times.
+    let lines: Vec<&str> = lines.unique();
+
+    print_lines(&lines, recursive)?;
+
+    Ok(())
+}
+
+fn print_lines(lines: &Vec<&str>, recursive: bool) -> Result<()> {
+    // When using the recursive option, we have lines that tells us what
+    // folder we are into. This regex match those lines so we can print them in color.
     let sub_dir_text = Regex::new(&String::from(r"^(.+)/*([^/]+)*:$")).unwrap();
 
-    // lock stdout manually for better performances since we are going to print
-    // to it a lot
+    // Avoid flushing each time we print a line.
     let stdout = std::io::stdout();
     let mut lock = stdout.lock();
 
-    // remove items that appears multiple times
-    let lines: Vec<&str> = lines.unique();
+    const GREEN: &str = "\x1b[92m";
+    const RESET: &str = "\x1b[0m";
 
     for line in lines {
-        if *recursive && sub_dir_text.is_match(line) {
-            writeln!(lock, "{}{}{}", colors::GREEN, line, colors::RESET)?;
+        if recursive && sub_dir_text.is_match(line) {
+            writeln!(lock, "{}{}{}", GREEN, line, RESET)?;
         } else {
             writeln!(lock, "{}", line)?;
         }
     }
+
     Ok(())
 }
