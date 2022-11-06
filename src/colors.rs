9f58420f2a21119c06d8fe50dc9ad7ef7a2e7444
gmnsii <95436780+gmnsii@users.noreply.github.com>
2022-11-06 14:23:47 -0800

    Delete colors.rs

--- src/colors.rs
+++
@@ -1,7 +0,0 @@
-//! Color constants
-
-pub const RED: &str = "\x1b[91m";
-pub const GREEN: &str = "\x1b[92m";
-pub const YELLOW: &str = "\x1b[93m";
-pub const BLUE: &str = "\x1b[94m";
-pub const RESET: &str = "\x1b[0m";
