9166d718e47265f63b3fbfa932bccaff7f4d2c11
gmnsii <95436780+gmnsii@users.noreply.github.com>
2022-11-06 14:24:32 -0800

    Update ls.rs

--- src/ls.rs
+++ src/ls.rs
@@ -1,16 +1,12 @@
-//! functions to get all files (before filtering them) using ls
-
+use std::process;
 use std::process::Command;
 
-#[path = "./colors.rs"]
-mod colors;
 #[path = "./types.rs"]
 mod types;
 
 use types::Result;
 
-// Runs ls to get files
-pub fn run_ls(path: &str, all: &bool, recursive: &bool) -> Result<String> {
+pub fn run_ls(path: &str, all: bool, recursive: bool) -> Result<String> {
     let args = get_ls_options(all, recursive);
 
     let output = Command::new("ls").arg(args).arg(path).output();
@@ -18,20 +14,11 @@ pub fn run_ls(path: &str, all: &bool, recursive: &bool) -> Result<String> {
     match output {
         Ok(content) => output_to_str(content),
 
-        Err(e) => {
-            eprintln!(
-                "{} permscan: ls: failed to get files: {}. is ls installed ?{}",
-                colors::RED,
-                e,
-                colors::RESET
-            );
-            Err("".into())
-        }
+        Err(e) => Err(format!("ls: failed to get files: {}. is ls installed ?", e).into()),
     }
 }
 
-// Determines ls options based on permscan options.
-fn get_ls_options(all: &bool, recursive: &bool) -> String {
+fn get_ls_options(all: bool, recursive: bool) -> String {
     let ls_options = String::from("-lh")
         + match all {
             true => "a",
@@ -39,23 +26,16 @@ fn get_ls_options(all: &bool, recursive: &bool) -> String {
         };
     let ls_options = ls_options
         + match recursive {
-            true => {
-                println!("{}Please be patient, a recursive search can take time... {}", colors::BLUE, colors::RESET);
-                "R"
-            }
+            true => "R",
             false => "",
         };
     ls_options
 }
 
-// Converts a command output to a String
-fn output_to_str(output: std::process::Output) -> Result<String> {
+fn output_to_str(output: process::Output) -> Result<String> {
     let stdout = String::from_utf8(output.stdout);
     match stdout {
         Ok(string) => Ok(string),
-        Err(e) => {
-            eprintln!("permscan: ls: failed to parse ls output: {}", e);
-            Err("".into())
-        }
+        Err(e) => return Err(format!("ls: failed to parse ls output: {}", e).into()),
     }
 }
