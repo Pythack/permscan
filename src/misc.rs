ad424d9fbd9c18fe378ffd53ff3b060edd61c252
gmnsii <95436780+gmnsii@users.noreply.github.com>
2022-11-06 14:24:58 -0800

    Update misc.rs

--- src/misc.rs
+++ src/misc.rs
@@ -1,32 +1,28 @@
-//! miscellaneous functions
-
-
-#[path = "./colors.rs"]
-mod colors;
 #[path = "./types.rs"]
 mod types;
 
+use std::path::Path;
 use types::Result;
 
-// Removes first character from string
-pub fn rem_first(string: &str) -> &str {
+pub fn remove_first_char(string: &str) -> &str {
     let mut chars = string.chars();
     chars.next();
     chars.as_str()
 }
 
-// Verifies if any types arguments given by the user is valid
 pub fn verify_type_argument(type_arg: &str) -> Result<()> {
     let possible_types = ["-", "d", "b", "c", "p", "l", "s"];
 
     if !possible_types.contains(&type_arg) {
-        eprintln!(
-            "{}permscan: {}: invalid type argument{}",
-            colors::RED,
-            type_arg,
-            colors::RESET
-        );
-        return Err("typeArgErr".into());
+        return Err(format!("`{}`: invalid type argument", type_arg).into());
+    }
+    Ok(())
+}
+
+pub fn check_path_exists(path: &str) -> Result<()> {
+    let path_exists = Path::new(&path).exists();
+    if !path_exists {
+        return Err(format!("No such file or directory: `{}`", path).into());
     }
     Ok(())
 }
