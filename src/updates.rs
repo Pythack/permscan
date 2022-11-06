194bcefd481335c990a8e7c15d6dfb3b928b0db4
gmnsii <95436780+gmnsii@users.noreply.github.com>
2022-11-06 14:25:50 -0800

    Update updates.rs

--- src/updates.rs
+++ src/updates.rs
@@ -1,5 +1,3 @@
-//! Function to update permscan
-
 use reqwest::blocking::Client;
 use std::io::{self, stdout, Write};
 use subprocess::Exec;
@@ -11,21 +9,13 @@ mod types;
 
 use types::Result;
 
-use crate::colors;
-
-// Get the current version from cargo.toml
+// Gets the current version from the Cargo.toml file.
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-// Check for a newer version and if one exists, call ask_for_update()
-pub fn check_for_newer_version(build: &bool) -> Result<()> {
-    println!(
-        "{}Current version: {}{}",
-        colors::BLUE,
-        VERSION,
-        colors::RESET
-    );
+pub fn check_for_newer_version(build: bool) -> Result<()> {
+    info!("Current version: {}", VERSION);
     print!("Checking latest version on GitHub... ");
-    let _flush = io::stdout().flush();
+    let _flush = stdout().flush();
 
     let api_response = request_latest_version();
 
@@ -36,36 +26,31 @@ pub fn check_for_newer_version(build: &bool) -> Result<()> {
             let latest = json_to_vec(json)?;
             if !latest.is_empty() {
                 // compare latest release to current version
-                if misc::rem_first(latest[0]["tag_name"].as_str().unwrap())
-                    != VERSION
-                {
-                    println!(
-                        "\r{}Newer version available: {}! Visit this url: {}{}",
-                        colors::YELLOW,
-                        misc::rem_first(
-                            latest[0]["tag_name"].as_str().unwrap()
-                        ),
+                if misc::remove_first_char(latest[0]["tag_name"].as_str().unwrap()) != VERSION {
+                    info!(
+                        "Newer version available: {}! Visit this url: {}",
+                        misc::remove_first_char(latest[0]["tag_name"].as_str().unwrap()),
                         latest[0]["html_url"].as_str().unwrap(),
-                        colors::RESET
                     );
                     ask_for_update(build)?
                 } else {
-                    println!("\r{}You have the latest version! Thank you for using permscan!{}", colors::GREEN, colors::RESET);
+                    info!("You have the latest version! Thank you for using permscan!");
                 }
             }
         }
 
         Err(e) => {
-            eprintln!("\n{}permscan: update: failed to connect to the github api: {}. are you connected to the internet ?{}", colors::RED, e, colors::RESET);
-            return Err("connectionErr".into());
+            return Err(format!(
+                "updater: failed to connect to the github api: {}. are you connected to the internet ?",
+                e
+            )
+            .into());
         }
     }
     Ok(())
 }
 
-// Send a get request for the latest version of permscan to github
-fn request_latest_version(
-) -> std::result::Result<reqwest::blocking::Response, reqwest::Error> {
+fn request_latest_version() -> std::result::Result<reqwest::blocking::Response, reqwest::Error> {
     let client = Client::new();
     client
         .get("https://api.github.com/repos/Pythack/permscan/releases")
@@ -73,87 +58,57 @@ fn request_latest_version(
         .send()
 }
 
-// Wrapper around reqwest::blocking::Response.text() that allows
-// to print a custom message when the Result is an error
 fn response_to_str(response: reqwest::blocking::Response) -> Result<String> {
     return match response.text() {
         Ok(str) => Ok(str),
         Err(e) => {
-            eprintln!(
-                "\n{}permscan: update: failed to parse github api response: {}{}",
-                colors::RED,
-                e,
-                colors::RESET
-            );
-            return Err("parsingErr".into());
+            return Err(format!("updater: failed to parse github api response: {}", e).into());
         }
     };
 }
 
-// Wrapper around serde_json::from_str() that allows to print a custom message
-// when the Result is an error
 fn str_to_json(str: String) -> Result<serde_json::Value> {
     let json: serde_json::Value = match serde_json::from_str(&str) {
         Ok(json) => json,
         Err(e) => {
-            eprintln!(
-                "\n{}permscan: update: failed to parse github api response: {}{}",
-                colors::RED,
-                e,
-                colors::RESET
-            );
-            return Err("parsingErr".into());
+            return Err(format!("updater: failed to parse github api response: {}", e).into());
         }
     };
     Ok(json)
 }
 
-// Wrapper around json.as_array() that returns an error when the Option
-// is None
 fn json_to_vec(json: serde_json::Value) -> Result<Vec<serde_json::Value>> {
     return match json.as_array() {
         Some(val) => Ok(val.to_vec()),
-        None => {
-            eprintln!(
-                "\n{}permscan: update: failed to parse github api response{}",
-                colors::RED,
-                colors::RESET
-            );
-            Err("parsingErr".into())
-        }
+        None => Err("updater: failed to parse github api response".into()),
     };
 }
 
-// Asks the user if he wants to update
-fn ask_for_update(build: &bool) -> Result<()> {
+fn ask_for_update(build: bool) -> Result<()> {
     print!("Do you want to update ? (y/*) ");
     let mut answer = String::new();
     get_input(&mut answer)?;
     if answer.to_lowercase().trim() == "y" {
-        if let Err(e) = update(*build) {
-            eprintln!("{}{}{}", colors::RED, e, colors::RESET);
-            return Err("updateErr".into());
+        if let Err(e) = update(build) {
+            return Err(e);
         }
     }
     Ok(())
 }
 
-// Wrapper around io::stdin().read_line() that retry when failing
 fn get_input(buffer: &mut String) -> Result<&str> {
     let _flush = stdout().flush();
     match io::stdin().read_line(buffer) {
         Ok(_) => Ok(buffer),
         Err(e) => {
-            eprintln!(
-                "\n{}permscan: update: failed to read input: {}. please retry: {}\n", colors::RED, e, colors::RESET
-            );
+            error!("\nupdater: failed to read input: {}. please retry: \n", e,);
             get_input(buffer)
         }
     }
 }
 
-// Use permscan-installer to install the newest
-// version (overwrite the current version)
+// Uses permscan-installer to install
+// the newest version (overwrite the current version).
 fn update(build: bool) -> Result<()> {
     Exec::shell("wget https://raw.githubusercontent.com/Pythack/permscan/master/permscan-installer.sh").join()?;
     Exec::shell("chmod +x ./permscan-installer.sh").join()?;
